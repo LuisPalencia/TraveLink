461243520fa66333f823978b408bcdea08db02d0
luispalencia25 <luis.palencia525@gmail.com>
2022-03-09 16:31:36 +0100

    [WIP] - Added method that returns a calendar with the time at 00:00:00

--- app/src/main/java/com/luispalenciadelcampo/travelink/utils/GenericFunctions.kt
+++ app/src/main/java/com/luispalenciadelcampo/travelink/utils/GenericFunctions.kt
@@ -49,6 +49,16 @@ class GenericFunctions {
             return calendar
         }
 
+        fun getActualCalendarWithNoHour(): Calendar{
+            val calendar = Calendar.getInstance()
+            calendar.set(Calendar.HOUR_OF_DAY, 0)
+            calendar.set(Calendar.MINUTE, 0)
+            calendar.set(Calendar.SECOND, 0)
+            calendar.set(Calendar.MILLISECOND, 0)
+
+            return calendar
+        }
+
 
 
 
